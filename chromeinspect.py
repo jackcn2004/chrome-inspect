c91225fad78166dcb9c6592851ed81e29fd24eb9
Ryan Nowakowski <ryan@keyingredient.com>
2015-09-28 13:50:25 -0500

    wait for ionic action

--- chromeinspect.py
+++ chromeinspect.py
@@ -3,15 +3,29 @@ from time import sleep
 
 from selenium import webdriver
 
+def find_ionic_inspect_action(driver):
+    browsers = driver.find_elements_by_class_name('browser')
+    for browser in browsers:
+        name = browser.find_element_by_class_name('browser-name')
+        if 'ionic' in name.text:
+            action = browser.find_element_by_class_name('action')
+            return action
+    return None
+
+
+def wait_for_ionic_inspect_action(driver):
+    while True:
+        action = find_ionic_inspect_action(driver)
+        if action:
+            return action
+        sleep(0.5)
+
+
 profile_dir = os.path.join(os.environ['HOME'], ".config/google-chrome")
 options = webdriver.ChromeOptions()
 options.add_argument("--user-data-dir=%s" % profile_dir)
 driver = webdriver.Chrome(chrome_options=options)
 driver.set_window_size(800, 400)
 driver.get("chrome://inspect/#devices")
-while True:
-    inspect = driver.find_element_by_class_name('action')
-    if inspect.text == 'inspect':
-        break
-    sleep(0.5)
-inspect.click()
+action = wait_for_ionic_inspect_action(driver)
+action.click()
